a2f64c7acbd48e159eb0fdd4d571c2e0f1970f54
liuyiyou <liuyiyou@ibalife.com>
2019-02-11 16:43:19 +0800

    SpringBoot属性配置

--- springboot-configuration/src/main/java/cn/liuyiyou/springboot/config/AddressConfig.java
+++ springboot-configuration/src/main/java/cn/liuyiyou/springboot/config/AddressConfig.java
@@ -1,24 +1,23 @@
 package cn.liuyiyou.springboot.config;
 
 import lombok.Data;
+import lombok.ToString;
 import lombok.experimental.Accessors;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-/***
- *
+/**
  * @author: liuyiyou.cn
- * @date: 2018/9/29
- * @Copyright 2018 liuyiyou.cn Inc. All rights reserved
+ * @date: 2019/2/11
+ * @version: V1.0
  */
 @Data
 @Accessors(chain = true)
 @Configuration
-@ConfigurationProperties(prefix = "user")
-public class UserConfig {
+@ConfigurationProperties(prefix = "address")
+public class AddressConfig {
 
-    private String name;
-    private int age;
     private String prov;
-
+    private String city;
+    private String county;
 }
